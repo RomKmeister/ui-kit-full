011b0c0951dc9189fb94ac91767dc82a9899806b
RomK <romank1988@mail.ru>
2019-07-17 02:59:37 +0300

    Блоки слайдера разделены в папки. Добавлены дата аттрибуты. Данные передаются независимо

--- src/slider-with-bubble/slider.js
+++ src/slider-with-bubble/slider.js
@@ -15,13 +15,4 @@ $( function() {
       });
 });
 
-$( function() {
-    $( ".slider-with-scale" ).slider({
-        value: 80,
-        classes: {
-            "ui-slider-handle": "sslider-with-scale__handle slider-with-scale__handle--green"
-        }
-    });
-});
-
 
---
+++ src/slider-with-scale/slider.js
@@ -0,0 +1,10 @@
+$( function() {
+    $( ".slider-with-scale" ).slider({
+        value: 80,
+        classes: {
+            "ui-slider-handle": "sslider-with-scale__handle slider-with-scale__handle--green"
+        }
+    });
+});
+
+

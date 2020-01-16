0bd137430bd6752b7c9ab31a15360033f87c6862
RomK <romank1988@mail.ru>
2020-01-16 17:04:06 +0300

    Замена span на div в подсказке

--- src/slider-with-bubble/slider.js
+++ src/slider-with-bubble/slider.js
@@ -1,17 +1,17 @@
 $( function() {
-    $( ".slider-with-bubble" ).each(function() {
-        $( this ).slider({
-            value: $( this ).data("value"),
-            min: $( this ).data("min"),
-            max: $( this ).data("max"),
+    $(".slider-with-bubble").each(function() {
+        $(this).slider({
+            value: $(this).data("value"),
+            min: $(this).data("min"),
+            max: $(this).data("max"),
             classes: {
             "ui-slider-handle": "slider-with-bubble__handle slider-with-bubble__handle_orange"
             },
             create: function() {
-                $( this ).find(".slider-with-bubble__handle").html( "<span class='slider-with-bubble__buble'>" + $( this ).slider( "value" ) + "</span>");
+                $(this).find(".slider-with-bubble__handle").html( "<div class='slider-with-bubble__buble'>" + $(this).slider("value") + "</div>");
             },
             slide: function( event, ui ) {
-                $( this ).find(".slider-with-bubble__handle").html( "<span class='slider-with-bubble__buble'>" + ui.value  + "</span>");
+                $(this).find(".slider-with-bubble__handle").html( "<div class='slider-with-bubble__buble'>" + ui.value  + "</div>");
             }
         });
       });

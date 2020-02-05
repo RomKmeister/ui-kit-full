4d8f570a21677032dd2f033d39e18d40e073a1d1
RomKmeister <romank1988@mail.ru>
2020-02-06 02:06:11 +0300

    Добавлена фавиконка. ДОбавление шаблона с своим страницам. Футер прижат к низу окна

--- webpack.config.js
+++ webpack.config.js
@@ -43,22 +43,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.pug',
+      favicon: 'src/favicon.ico'
     }),
     new HtmlWebpackPlugin({
       filename: 'main-page.html',
       template: 'src/pages/main-page/main-page.pug',
+      favicon: 'src/favicon.ico'
     }),
     new HtmlWebpackPlugin({
       filename: 'event-item.html',
       template: 'src/pages/event-item/event-item.pug',
+      favicon: 'src/favicon.ico'
     }),
     new HtmlWebpackPlugin({
       filename: 'choosing-seat.html',
       template: 'src/pages/choosing-seat/choosing-seat.pug',
+      favicon: 'src/favicon.ico'
     }),
     new HtmlWebpackPlugin({
       filename: 'customer-info.html',
       template: 'src/pages/customer-info/customer-info.pug',
+      favicon: 'src/favicon.ico'
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',

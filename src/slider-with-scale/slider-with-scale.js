dbc22fd435dcba9e4d522597da16c81b638fda36
RomKmeister <romank1988@mail.ru>
2020-03-02 13:32:39 +0300

    Табы заменены на пробелы

--- src/slider-with-scale/slider-with-scale.js
+++ src/slider-with-scale/slider-with-scale.js
@@ -1,15 +1,15 @@
 $( function() {
-    $(".js-slider-with-scale").each(function() {
-        $(this).slider({
-            value: $(this).data("value"),
-            min: $(this).data("min"),
-            max: $(this).data("max"),
-            range: "min",
-            classes: {
-                "ui-slider-range": "slider-with-scale__range_green",
-                "ui-slider-handle": "slider-with-scale__handle slider-with-scale__handle_green"
-            }
-        });
-      });
+  $(".js-slider-with-scale").each(function() {
+    $(this).slider({
+      value: $(this).data("value"),
+      min: $(this).data("min"),
+      max: $(this).data("max"),
+      range: "min",
+      classes: {
+        "ui-slider-range": "slider-with-scale__range_green",
+        "ui-slider-handle": "slider-with-scale__handle slider-with-scale__handle_green"
+      }
+    });
+   });
 });
 

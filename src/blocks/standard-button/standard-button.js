0c67b5055e6bdee20ef7f9c53d79a3094a72d2e1
RomKmeister <romank1988@mail.ru>
2020-03-10 02:11:00 +0300

    Исправлено название блока. Добавлена деструктуризация и проверка дефольных значений

--- src/blocks/standard-button/standard-button.js
+++ src/blocks/standard-button/standard-button.js
@@ -2,13 +2,13 @@
 
     'use strict';
   
-    const $ripple = $('.js-button__ripple');
+    const $ripple = $('.js-standard-button__ripple');
   
     $ripple.on('click.ui.ripple', function(e) {
   
       const $this = $(this);
       const $offset = $this.parent().offset();
-      const $circle = $this.find('.button__ripple_circle');
+      const $circle = $this.find('.standard-button__ripple_circle');
   
       const x = e.pageX - $offset.left;
       const y = e.pageY - $offset.top;
@@ -18,12 +18,12 @@
         left: x + 'px'
       });
   
-      $this.addClass('button__ripple_active');
+      $this.addClass('standard-button__ripple_active');
   
     });
   
     $ripple.on('animationend webkitAnimationEnd oanimationend MSAnimationEnd', function(e) {
-        $(this).removeClass('button__ripple_active');
+        $(this).removeClass('standard-button__ripple_active');
     });
   
   })(jQuery, window, document);

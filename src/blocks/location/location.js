be269fa531bd3de251177f0f4986bcb1312204f8
RomKmeister <romank1988@mail.ru>
2020-03-11 04:03:41 +0300

    Изображения перенесены в папки

--- src/blocks/location/location.js
+++ src/blocks/location/location.js
@@ -23,7 +23,7 @@ $( function() {
       }, {
         balloonContentLayout: CustomBalloon,
         iconLayout: 'default#image',
-        iconImageHref: '../src/map/map-pin.svg',
+        iconImageHref: '../src/location/images/map-pin.svg',
         iconImageSize: [41, 56],
         iconImageOffset: [-5, -38]
       });

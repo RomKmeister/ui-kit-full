da89002f76e6194ab362c6d576ebca71e43da02b
RomKmeister <romank1988@mail.ru>
2020-03-20 01:33:33 +0300

    Исправлены стили блока. Функция js заменена на класс

--- src/blocks/datepicker/datepicker.js
+++ src/blocks/datepicker/datepicker.js
@@ -1,15 +1,36 @@
-$( function() {
-  const $datepickerCalendar = $( '.js-datepicker__calendar');
-  $datepickerCalendar.each(function() {
-    $(this).datepicker({
+class Datepicker {
+  constructor() {
+    this.show();
+  }
+
+  show() {
+    this._findElements();
+    this.$calendar.datepicker({
       altField: '.js-datepicker__date',
       dateFormat: 'd',
       dayNamesMin: [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ],
       inline: true,
       firstDay: 1,
-      showButtonPanel: true,
       showOtherMonths: true,
-      constrainInput: true,
     });
+
+    this.$todayButton.on(`click`, this._handleTodayButtonClick.bind(this));
+  }
+
+  _findElements() {
+    this.$calendar = $( '.js-datepicker__calendar');
+    this.$todayButton = $( '.js-datepicker__button');
+  }
+
+  _handleTodayButtonClick() {
+    this.$calendar.datepicker('setDate', new Date());
+  }
+}
+
+$( () => {
+  const $calendar = $('.datepicker');
+
+  $calendar.each(() => {
+    new Datepicker();
   });
 });

75e1cd61516c250b127c0274d709dc4ade4844b9
RomKmeister <romank1988@mail.ru>
2020-03-20 16:10:27 +0300

    JS Замена функций на классы

--- src/blocks/drop-down/drop-down.js
+++ src/blocks/drop-down/drop-down.js
@@ -1,11 +1,23 @@
-$( function() {
-  const $dropDown = $('.js-drop-down__select');
-  $dropDown.each(function() {
-    $(this).selectmenu({
+class DropDown {
+  constructor(element) {
+    this.element = element;
+    this.run();
+  }
+
+  run() {
+    const $dropDown = $('.js-drop-down__select');
+    $dropDown.selectmenu({
       width: '100%',
       classes: {
         'ui-selectmenu-menu': 'ui-selectmenu-menu_ui'
       }
     });
+  }
+}
+
+$(() => {
+  const $dropDown = $('.js-drop-down');
+  $dropDown.each((index, item) => {
+    new DropDown($(item))
   });
 });

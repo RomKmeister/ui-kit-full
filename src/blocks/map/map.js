3cf99852b6e9d3fdf312a0730a589b254969f049
RomKmeister <romank1988@mail.ru>
2020-04-15 18:47:09 +0300

    Координаты и адрес вынесены в параметры миксина. Панель нвынесена в паг #16

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -1,5 +1,4 @@
 /* global ymaps */
-/* eslint comma-dangle: ["error", "never"] */
 
 class Map {
   constructor(element) {
@@ -8,35 +7,21 @@ class Map {
   }
 
   init() {
-    const $newMap = this.element.find('.js-map__map');
+    const $newMap = this.element.find('.js-map__body');
     ymaps.ready(() => {
       const myMap = new ymaps.Map($newMap[0], {
-        center: [37.787509, -122.444838],
+        center: $newMap.data('coordinates'),
         zoom: 13,
-        controls: []
-      }, {
-        searchControlProvider: 'yandex#search',
-        balloonCloseButton: false,
-        balloonPanelMaxMapArea: Infinity
-
+        controls: [],
       });
-      const CustomBalloon = ymaps.templateLayoutFactory.createClass(
-        `<div class='map__panel'>
-        <div class='map__greetings'>Meet us!</div>
-        <div class='map__address'>1259 California st San Francisco, CA</div>
-        <div class='map__mylocation'></div>
-        <div class='map__pin'></div>`
-      );
-      const myPlacemark = new ymaps.Placemark([37.787509, -122.444838], {
+      const myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
       }, {
-        balloonContentLayout: CustomBalloon,
         iconLayout: 'default#image',
-        iconImageHref: '/../images/map-pin.svg',
+        iconImageHref: '../src/blocks/map/images/map-pin.svg',
         iconImageSize: [41, 56],
-        iconImageOffset: [-5, -38]
+        iconImageOffset: [-5, -38],
       });
       myMap.geoObjects.add(myPlacemark);
-      myPlacemark.balloon.open();
     });
   }
 }
